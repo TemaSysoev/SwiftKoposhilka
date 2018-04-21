10bd5020e0d6f414e82dc3c85657c9d09849e669
TemaSysoev <artyom.sysoev@gmail.com>
2018-04-21 17:15:04 +0300

    fix Timer
    objc

--- Timer.swift
+++ Timer.swift
@@ -2,5 +2,5 @@
 var timer = Timer
 timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
 
-func updateTimer(){
+@objc func updateTimer(){
 }
