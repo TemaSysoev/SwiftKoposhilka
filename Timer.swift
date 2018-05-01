3badd5d33de42b5ac4d5083ea8e0cd96c6c5d39d
TemaSysoev <artyom.sysoev@gmail.com>
2018-05-01 13:41:06 +0300

    Timer
    Update

--- Timer.swift
+++ Timer.swift
@@ -1,5 +1,5 @@
 //Timer
-var timer = Timer
+var timer = Timer()
 timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
 
 @objc func updateTimer(){
