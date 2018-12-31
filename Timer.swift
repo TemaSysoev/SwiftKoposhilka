f6d9f44346d5bb2437729873033b62f55762466f
TemaSysoev <artyom.sysoev@gmail.com>
2018-12-31 21:08:45 +0300

    viewDidLoad added

--- Timer.swift
+++ Timer.swift
@@ -1,6 +1,9 @@
 //Timer
 var timer = Timer()
-timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
 
+//override func viewDidLoad() {
+//  super.viewDidLoad()
+timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+//}
 @objc func updateTimer(){
 }
